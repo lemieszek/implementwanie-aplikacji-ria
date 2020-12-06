f0e63ca505882dc468f808f96d9080c4fe034ff0
Dawid Lemieszek <37555757+lemieszek@users.noreply.github.com>
2020-12-06 09:58:22 +0100

    Ajax + ASP.NET Web API lab

---
+++ lab/lab.05 Ajax + ASP.NET Web API/Controllers/MathController.cs
@@ -0,0 +1,27 @@
+﻿using ASP_01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_01.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MathController : ControllerBase
+    {
+        [HttpGet]
+        public CalcResult Calculate(int x, int y)
+        {
+            return new CalcResult()
+            {
+                Sum = x + y,
+                Difference = x - y,
+                Product = x * y,
+                Quotient = x / y
+            };
+        }
+    }
+
+}
